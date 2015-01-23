f47d49af2c3c8e3c8037cdb69ce5d0d2e882bbe2
Francis Tseng <f+accounts@frnsys.com>
2015-01-23 14:13:47 -0500

    vump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lateral',
-    version='0.0.1',
+    version='0.0.2',
     description='python interface to Lateral\'s APIs',
     url='https://github.com/ftzeng/lateral',
     author='Francis Tseng',
